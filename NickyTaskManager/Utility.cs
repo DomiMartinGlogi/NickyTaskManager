dea5b4b906f2c067ea97ebf43c2111036317be78
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-01-12 13:01:26 +0100

    Added a utility class, entirely for serializing and deserializing JSON.
    No I do not intend to remove it.

---
+++ NickyTaskManager/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace NickyTaskManager;
+
+public static class Utility
+{   
+    internal static List<NickyTaskList> launcher()
+    {
+        var fileName = "Lists.json";
+        var jsonString = File.ReadAllText(fileName);
+        List<NickyTaskList> list = JsonSerializer.Deserialize<List<NickyTaskList>>(jsonString);
+        return list;
+    }
+
+    internal static void saver(List<NickyTaskList> list)
+    {
+        var jsonString = JsonSerializer.Serialize(list);
+        var fileName = "Lists.json";
+        File.WriteAllText(fileName,jsonString);
+    }
+}
