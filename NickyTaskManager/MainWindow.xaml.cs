65940be7869a39953e308fb47a9fa6dd30038f77
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-01-13 15:05:28 +0100

    Added ability to manage tasks, minor issues persist.

--- NickyTaskManager/MainWindow.xaml.cs
+++ NickyTaskManager/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.JavaScript;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace NickyTaskManager
 {
@@ -12,12 +13,19 @@ namespace NickyTaskManager
     public partial class MainWindow : Window
     {
         private List<NickyTaskList> list;
-        private List<String> nameList;
+        private List<String> nameListTaskLists;
+        private List<String> nameListTasks;
         private List<String> priority;
         private int indexOfList;
         private int indexOfTask;
+        private DispatcherTimer dispatcherTimer;
         public MainWindow()
         {
+            //  DispatcherTimer setup
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,0,1);
+            dispatcherTimer.Start();
             InitializeComponent();
             Window_Loaded();
 
@@ -38,21 +46,34 @@ namespace NickyTaskManager
 
         private void Update()
         {
-            nameList = new List<string>();
+            /*
+             * I believe that this method, at some point
+             * Breaks something in the display of the Task selection combobox,
+             * I do not why that is, but it displays Tasks from all lists
+             */
+            nameListTasks = new List<string>();
+            nameListTaskLists = new List<string>();
+            NickyTaskList tempTaskList;
             foreach (NickyTaskList subList in list)
             {
-                nameList.Add(subList.Name);
+                nameListTaskLists.Add(subList.Name);
             }
-            ListSelect.ItemsSource = nameList;
+            ListSelect.ItemsSource = nameListTaskLists;
             int index = -1;
             foreach (NickyTaskList sublist in list)
             {
                 if (sublist.Name == ListSelect.Text)
                 {
+                    foreach (var task in sublist.list)
+                    {
+                        nameListTasks.Add(task.Task);
+                    }
                     index = list.IndexOf(sublist);
                     break;
                 }
             }
+            
+            ItemSelect.ItemsSource = nameListTasks;
         }
 
         private void SaveButton_OnClick(object sender, RoutedEventArgs e)
@@ -98,20 +119,45 @@ namespace NickyTaskManager
 
         private void ListSelect_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            try
+            Update();
+        }
+
+        private void CreateTask_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (New_Task.Text == "" || New_Task.Text == "New Task")
             {
-                NickyTaskList taskList = list[indexOfList];
-                List<String> taskNames = new List<string>();
-                foreach (NickyManagedTask task in taskList.list)
+                Console.WriteLine("Invalid Task Name");
+                return;
+            }
+            else
+            {
+                if (DueDateNew.SelectedDate < DateTime.Now)
                 {
-                    taskNames.Add(task.Task);
+                    Console.WriteLine("Date cannot be in the past");
+                    return;
                 }
-                ItemSelect.ItemsSource = taskNames;
+                else
+                {
+                    Priority priority = Priority.Text switch
+                    {
+                        "Very High" => NickyTaskManager.Priority.VeryHigh,
+                        "High" => NickyTaskManager.Priority.High,
+                        "Medium" => NickyTaskManager.Priority.Medium,
+                        "Low" => NickyTaskManager.Priority.Low,
+                        "Very Low" => NickyTaskManager.Priority.VeryLow,
+                        _ => NickyTaskManager.Priority.Default
+                    };
+                    NickyManagedTask newTask = new NickyManagedTask(New_Task.Text, priority, (DateTime)DueDateNew.SelectedDate);
+                    NickyTaskList taskList = list[indexOfList];
+                    taskList.addTask(newTask);
+                }
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine("List not found.");
-            }
+            Update();
+            New_Task.Text = "New Task";
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Update();
         }
     }
 }
