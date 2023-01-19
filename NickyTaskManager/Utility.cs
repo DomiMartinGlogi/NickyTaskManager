225f75c7ca8c8269eb507ef773635e7310369fed
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-01-19 10:46:53 +0100

    Adjusted the launcher() and saver() methods.

--- NickyTaskManager/Utility.cs
+++ NickyTaskManager/Utility.cs
@@ -10,18 +10,28 @@ public static class Utility
     internal static List<NickyTaskList> launcher()
     {
         var fileName = "Lists.json";
+        List<NickyTaskList> list;
+        List<NickyTaskList> completeList = new List<NickyTaskList>();
         try
         {
             var jsonString = File.ReadAllText(fileName);
-            List<NickyTaskList> list = JsonSerializer.Deserialize<List<NickyTaskList>>(jsonString);
-            return list;
+            list = JsonSerializer.Deserialize<List<NickyTaskList>>(jsonString);
         }
         catch (Exception e)
         {
             Console.WriteLine("A list wasn't found, a new list will be generated.");
-            List<NickyTaskList> list = new List<NickyTaskList>();
+            list = new List<NickyTaskList>();
             return list;
         }
+
+        foreach (var sublist in list)
+        {
+            var filename = sublist.Name + ".json";
+            NickyTaskList newSublist = JsonSerializer.Deserialize<NickyTaskList>(fileName);
+            completeList.Add(newSublist);
+        }
+
+        return completeList;
     }
 
     internal static void saver(List<NickyTaskList> list)
@@ -29,5 +39,10 @@ public static class Utility
         var jsonString = JsonSerializer.Serialize(list);
         var fileName = "Lists.json";
         File.WriteAllText(fileName,jsonString);
+        foreach (var sublist in list)
+        {
+            var fileNameSublist = sublist.Name;
+            var jsonStringSublist = JsonSerializer.Serialize(sublist.list);
+        }
     }
 }
